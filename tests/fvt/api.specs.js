be8244cd981a5ed70876a5379a504d0243ea0fcf
jls68 <48459052+jls68@users.noreply.github.com>
2019-05-17 03:04:49 +1200

    Change to city instead of zip

--- tests/fvt/api.specs.js
+++ tests/fvt/api.specs.js
@@ -15,21 +15,21 @@
 
     describe('Get Weather', function() {
 
-    	it('with valid zip code', function(done) {
+    	it('with valid town name', function(done) {
         if(!appUrl) {
             assert.fail("Environment variable APP_URL is not defined");
             return done();
         }
         request({
       		method: 'GET',
-              url: appUrl + '/api/v1/getWeather?zip=78613'
+              url: appUrl + '/api/v1/getWeather?q=Auckland'
           }, function(err, resp, body) {
           	if(err) {
           		assert.fail('Failed to get the response');
           	} else {
               assert.equal(resp.statusCode, 200);
               var pbody = JSON.parse(body);
-              assert((pbody.city === 'Anderson Mill') || (pbody.city === 'Round Rock'), "City name does not match");
+              assert((pbody.city === 'Auckland') || (pbody.city === 'Auckland,nz'), "City name does not match");
               done();
             }
         });
@@ -53,21 +53,21 @@
         });
     	});
 
-      it('with another valid zip code', function(done) {
+      it('with another valid city name', function(done) {
         if(!appUrl) {
             assert.fail("Environment variable APP_URL is not defined");
             return done();
         }
         request({
       		method: 'GET',
-              url: appUrl + '/api/v1/getWeather?zip=78641'
+              url: appUrl + '/api/v1/getWeather?q=Taupo'
           }, function(err, resp, body) {
           	if(err) {
           		assert.fail('Failed to get the response');
           	} else {
               assert.equal(resp.statusCode, 200);
               var pbody = JSON.parse(body);
-              assert(pbody.city === 'Round Rock', "City name does not match");
+              assert(pbody.city === 'Taupo', "City name does not match");
               done();
             }
         });
