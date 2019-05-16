ec6617803ef46accec0b968ae169071e3e6aaf72
jls68 <48459052+jls68@users.noreply.github.com>
2019-05-17 03:06:12 +1200

    Update api.specs.js

--- tests/fvt/api.specs.js
+++ tests/fvt/api.specs.js
@@ -15,7 +15,7 @@
 
     describe('Get Weather', function() {
 
-    	it('with valid town name', function(done) {
+    	it('with valid city name', function(done) {
         if(!appUrl) {
             assert.fail("Environment variable APP_URL is not defined");
             return done();
@@ -35,7 +35,7 @@
         });
     	});
 
-      it('without zip code', function(done) {
+      it('without city name', function(done) {
         if(!appUrl) {
             assert.fail("Environment variable APP_URL is not defined");
             return done();
